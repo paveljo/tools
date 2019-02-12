86c197c35c47ec8084830a74d54392d7ce7f01f8
paveljo <47556319+paveljo@users.noreply.github.com>
2019-02-12 12:25:41 +0200

    Add files via upload

---
+++ WiseTools.framework/Headers/WiseTools.h
@@ -0,0 +1,20 @@
+//
+//  WiseTools.h
+//  WiseTools
+//
+//  Created by Pavel Joffe on 21/07/2016.
+//  Copyright © 2016 WiseSec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <WiseTools/CommonCrypto.h>
+#import <WiseTools/sqlite3.h>
+
+//! Project version number for WiseTools.
+FOUNDATION_EXPORT double WiseToolsVersionNumber;
+
+//! Project version string for WiseTools.
+FOUNDATION_EXPORT const unsigned char WiseToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WiseTools/PublicHeader.h>
+
